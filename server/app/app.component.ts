5802e5179d46fe7209f5991ac641bede556dd9bd
duxiaodong <duxiaodong@darlin.me>
2016-10-08 21:09:27 +0800

    update example

--- server/app/app.component.ts
+++ server/app/app.component.ts
@@ -13,15 +13,19 @@ export class AppComponent implements OnInit {
   text: string = ''
   cpText1: string = '春江潮水连海平'
   cpText2: string = '海上明月共潮生'
-  onClip1(e) {
+  onClip1(copyed) {
     this.text = this.cpText1
-    onClip(2)
+    onClip(copyed)
   }
-  onClip2(e) {
+  onClip2(copyed) {
     this.text = this.cpText2
-    onClip(e)
+    onClip(copyed)
   }
-  ngOnInit() { }
+  ngOnInit() {
+    setTimeout(() => {
+      this.cpText1 = 'chun jiang chao shui lian hai ping'
+    }, 9580)
+  }
 }
 
 function onClip(e) {
